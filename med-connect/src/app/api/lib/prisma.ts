0dda6b7504826ce673dd6e3c95fd73897a3481cf
owaisnaeem-coding <0hussainnaeem786@gmail.com>
2026-04-02 22:19:43 +0500

    some change in prisma.ts

--- med-connect/src/app/api/lib/prisma.ts
+++ med-connect/src/app/api/lib/prisma.ts
@@ -1,7 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  try {
+    return new PrismaClient({
+      adapter,
+      log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    });
+  } catch (error) {
+    console.error("Failed to instantiate Prisma Client:", error);
+    throw error;
+  }
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
